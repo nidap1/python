ff2b77343d1206979bef1dae6f324a0821a8c0f7
nida <nidap752@gmail.com>
2025-03-23 16:00:49 +0530

    Add files via upload

---
+++ #opration.py
@@ -0,0 +1,23 @@
+#opration
+a="raj"
+print(a)
+print(type(a))
+
+b=(1,2,3)
+print(b)
+print(type(b))
+
+c=['nida','roshani',1]
+print(c)
+print(type(c))
+
+
+g="raj"
+h="kadam"
+n=g+h
+print(n)
+
+
+m='mY nAmE iS RAjeNdrA and i am DA Trainer'
+r=a[13]+a[8]+a[32]+a[6]+a[3]+a[16]+a[17]+a[18]
+print(r)

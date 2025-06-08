264ac18e33db710b13359008577c1cb2318c689b
nida <nidap752@gmail.com>
2025-06-08 12:07:10 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,11 @@
+
+'''
+1. Program to print maximum among two numbers. 
+'''
+
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+if a>b:
+   print('a is maximum')
+else:
+    print('b is maximum')
